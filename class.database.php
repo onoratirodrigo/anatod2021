3393f6faa6d1a8e1daff0fc8230efcb6633d2ccc
Juan Pablo Larrea <juanlarrea@gmail.com>
2019-04-08 20:21:50 -0300

    Add files via upload

---
+++ class.database.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * anatod ® - ©
+ */
+?>
+<?php
+class class_db {
+    PUBLIC  $conn=NULL;
+    
+    CONST user      =   'test',
+          pass      =   'test5678',
+          db        =   'test_anatod',
+          serverip  =   'consumos1.c75o4mima6rb.us-east-1.rds.amazonaws.com';
+    
+    public function __construct(){
+        if(!$this->conn){
+            try {
+                $this->conn = new mysqli(SELF::serverip,SELF::user,SELF::pass,SELF::db); 
+                $this->conn->set_charset("utf8");
+                if (!$this->conn) {die('No se pudo conectar.');}
+            } catch (Exception $exc) {
+                echo $exc->getTraceAsString();
+            }
+        }
+    }
+}
