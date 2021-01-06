8f815bb77c922a7532bb6d378b76bff5242e5671
Juan Pablo Larrea <juanlarrea@gmail.com>
2021-01-05 21:40:05 -0300

    Update class.database.php

--- class.database.php
+++ class.database.php
@@ -10,7 +10,7 @@ class class_db {
     CONST user      =   'test',
           pass      =   'test5678',
           db        =   'test_anatod',
-          serverip  =   'anatodtest.c75o4mima6rb.us-east-1.rds.amazonaws.com';
+          serverip  =   'anatod-test.c75o4mima6rb.us-east-1.rds.amazonaws.com';
     
     public function __construct(){
         if(!$this->conn){
